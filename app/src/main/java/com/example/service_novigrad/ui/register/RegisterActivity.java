c4ea8c1a17b7f9c7f30a185c1d29058bc6a99f73
Rhiannon@07 <71230352+m3alij@users.noreply.github.com>
2020-10-06 22:43:56 -0400

    Added error alerts in the register text fields

--- app/src/main/java/com/example/service_novigrad/ui/register/RegisterActivity.java
+++ app/src/main/java/com/example/service_novigrad/ui/register/RegisterActivity.java
@@ -34,6 +34,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity{
+    //Declaring Variables
     private EditText editTextUsername;
     private EditText editTextPassword;
     private EditText editTextFirstName;
@@ -49,7 +50,7 @@ public class RegisterActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
+        //Assigning Variables
         editTextFirstName = findViewById(R.id.firstname);
         editTextLastName = findViewById(R.id.lastname);
         editTextUsername = findViewById(R.id.username);
@@ -61,7 +62,7 @@ public class RegisterActivity extends AppCompatActivity{
         buttonSubmit = findViewById(R.id.submit);
         radioGroup = (RadioGroup) findViewById(R.id.accountType);
 
-
+        //Assigning TextChangedListeners to the above variables.
         editTextUsername.addTextChangedListener(registerTextWatcher);
         editTextPassword.addTextChangedListener(registerTextWatcher);
         editTextFirstName.addTextChangedListener(registerTextWatcher);
@@ -69,7 +70,6 @@ public class RegisterActivity extends AppCompatActivity{
         editTextBranchID.addTextChangedListener(registerTextWatcher);
         employeeAccountTypeRadioButton.addTextChangedListener(registerTextWatcher);
         customerAccountTypeRadioButton.addTextChangedListener(registerTextWatcher);
-
 
 
 
@@ -115,8 +115,6 @@ public class RegisterActivity extends AppCompatActivity{
 
                 finish();
 
-
-
             }
         });
 
@@ -132,7 +130,11 @@ public class RegisterActivity extends AppCompatActivity{
             }
         });
     }
-
+    /*
+    TextWatcher, it's function is to look at the field in the register layout and only unlock the register button
+    after all the appropriate fields are inputted, it also serves as the function to set the error if the text fields inputted are
+    not according to format.
+    */
     private TextWatcher registerTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -141,29 +143,14 @@ public class RegisterActivity extends AppCompatActivity{
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            /*String usernameInput = editTextUsername.getText().toString().trim();
-            String passwordInput = editTextPassword.getText().toString().trim();
-            String firstNameInput = editTextFirstName.getText().toString().trim();
-            String lastNameInput = editTextLastName.getText().toString().trim();
-            int BranchIDInput = editTextBranchID.getId();
-
-            if(employeeAccountTypeRadioButton.isChecked()){
-                buttonSubmit.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty() && !lastNameInput.isEmpty() && BranchIDInput >= 0 && BranchIDInput <= 999 && passwordInput.length() > 5);
-            }
-            else if(customerAccountTypeRadioButton.isChecked()){
-                buttonSubmit.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty() && !lastNameInput.isEmpty() && passwordInput.length() > 5);
-            }*/
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
+            //Declaring and assigning the input from the user
             String usernameInput = editTextUsername.getText().toString().trim();
             String passwordInput = editTextPassword.getText().toString().trim();
             String firstNameInput = editTextFirstName.getText().toString().trim();
             String lastNameInput = editTextLastName.getText().toString().trim();
             String BranchIDInput = editTextBranchID.getText().toString().trim();
 
-
+            //Enables the button if true
             if(employeeAccountTypeRadioButton.isChecked()){
                 buttonSubmit.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty() && !lastNameInput.isEmpty() && BranchIDInput.length()==3 && passwordInput.length() > 5);
             }
@@ -171,7 +158,47 @@ public class RegisterActivity extends AppCompatActivity{
                 buttonSubmit.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty() && !lastNameInput.isEmpty() && passwordInput.length() > 5);
             }
         }
-    };
 
+        @Override
+        public void afterTextChanged(Editable s) {
+            /*
+            Sets an error in the text fields if they haven't been appropriatly addressed.
+             */
+            if(editTextFirstName.getText().toString().length() <= 0){
+                editTextFirstName.setError("First name can't be empty.");
+            }
+            else{
+                editTextFirstName.setError(null);
+            }
+
+            if(editTextLastName.getText().toString().length() <= 0){
+                editTextLastName.setError("Last name can't be empty.");
+            }
+            else{
+                editTextLastName.setError(null);
+            }
+
+            if(editTextUsername.getText().toString().length() <= 0){
+                editTextUsername.setError("Username can't be empty.");
+            }
+            else{
+                editTextUsername.setError(null);
+            }
+
+            if(editTextPassword.getText().toString().length() <= 5){
+                editTextPassword.setError("Password needs to be more than five characters.");
+            }
+            else{
+                editTextPassword.setError(null);
+            }
+
+            if(editTextBranchID.getText().toString().length() > 999 || editTextBranchID.getText().toString().length() < 100 && !customerAccountTypeRadioButton.isChecked()){
+                editTextBranchID.setError("BranchID needs to be exactly 3 digits.");
+            }
+            else{
+                editTextBranchID.setError(null);
+            }
+        }
+    };
 }
 
