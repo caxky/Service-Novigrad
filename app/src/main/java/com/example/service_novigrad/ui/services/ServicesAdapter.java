bf8eb6b97aca27d33e54a030df11b38aed5ece71
caxky <k_james7@outlook.com>
2020-10-28 19:48:48 -0400

    Created services tab with insert and delete functionality

---
+++ app/src/main/java/com/example/service_novigrad/ui/services/ServicesAdapter.java
@@ -0,0 +1,57 @@
+package com.example.service_novigrad.ui.services;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.service_novigrad.R;
+
+import java.util.ArrayList;
+
+public class ServicesAdapter extends RecyclerView.Adapter<ServicesAdapter.ViewHolder>{
+    private ArrayList<ServiceItem> mList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView1);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public ServicesAdapter(ArrayList<ServiceItem> list) {
+        mList = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.service_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ServiceItem currentItem = mList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+}
