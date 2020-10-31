34ffa6ca0f0500f96d5ab3f49e0f3fb21c34f8d9
Shang-Lin <38915581+Shang603@users.noreply.github.com>
2020-10-31 14:33:20 -0400

    Update ServiceItem.java
    Added FieldsAndAttachment instance variabe, its getter method, and added it into the constructor to implement default fields for services

--- app/src/main/java/com/example/service_novigrad/ui/services/ServiceItem.java
+++ app/src/main/java/com/example/service_novigrad/ui/services/ServiceItem.java
@@ -1,31 +1,47 @@
 package com.example.service_novigrad.ui.services;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Since we dont need image resources to be in the firebase we use @IgnoreExtraProperties + the
+// exclude annotation on the getter to ignore it when we push it into firebase
+@IgnoreExtraProperties
 public class ServiceItem {
-    private int mImageResource;
+
     private String mText1;
     private String mText2;
+    private FieldsAndAttachments fieldsAndAttachments = new FieldsAndAttachments();
+    private int mImageResource;
+
+//    private boolean firstName, lastName, maidenName, gender, nationality, POB, expiryDate, issueDate, DOB, signature, address, issuingAuthority, height, weight, bloodType, hairColour, eyeColour, classID, passport, birthCertificate, driversLicense, citzenshipCard, SIN, healthInsurance;
 
     public ServiceItem(int imageResource, String serviceName, String serviceType) {
         mImageResource = imageResource;
         mText1 = serviceName;
         mText2 = serviceType;
-
+        fieldsAndAttachments = new FieldsAndAttachments(serviceType);
     }
 
-    public void changeText1(String text){
+
+    public void changeText1(String text) {
         mText1 = text;
     }
 
+    @Exclude
     public int getImageResource() {
         return mImageResource;
     }
 
-    public String getServiceName(){
-        return mText1;
+    public String getServiceName() { return mText1;}
+
+    public String getServiceType() { return mText2;}
+
+    public FieldsAndAttachments getFieldsAndAttachments() {
+        return fieldsAndAttachments;
     }
 
-    public String getServiceType(){
-        return mText2;
-    }
 
 }
