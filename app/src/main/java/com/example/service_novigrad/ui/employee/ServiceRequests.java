43b9b7aeed98a15123fd3022b315a9fe2234409a
caxky <k_james7@outlook.com>
2020-11-16 19:49:27 -0500

    Created service requests panel for employee

---
+++ app/src/main/java/com/example/service_novigrad/ui/employee/ServiceRequests.java
@@ -0,0 +1,16 @@
+package com.example.service_novigrad.ui.employee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.service_novigrad.R;
+
+public class ServiceRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.employee_service_requests);
+    }
+}
