2af829682f963d2d8d11f4d7d92fe6717fb36235
caxky <k_james7@outlook.com>
2020-12-01 15:00:17 -0500

    Created initial branch service item files for customer branch services page

---
+++ app/src/main/java/com/example/service_novigrad/ui/customer/BranchServiceItem.java
@@ -0,0 +1,16 @@
+package com.example.service_novigrad.ui.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.service_novigrad.R;
+
+public class BranchServiceItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.branch_service_item);
+    }
+}
