253e6f80c6b868995308fc53399bff2073cfff92
Shang-Lin <38915581+Shang603@users.noreply.github.com>
2020-12-05 21:40:38 -0500

    Update AdminAccounts.java
    when deleting employee also delete corresponding branch

--- app/src/main/java/com/example/service_novigrad/ui/admin/AdminAccounts.java
+++ app/src/main/java/com/example/service_novigrad/ui/admin/AdminAccounts.java
@@ -12,6 +12,7 @@ import com.example.service_novigrad.accounts.CustomerAccount;
 import com.example.service_novigrad.accounts.EmployeeAccount;
 import com.example.service_novigrad.ui.accounts.AccountItem;
 import com.example.service_novigrad.ui.accounts.AccountsAdapter;
+import com.example.service_novigrad.ui.register.Branch;
 import com.example.service_novigrad.ui.services.ServiceItem;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -39,7 +40,7 @@ public class AdminAccounts extends AppCompatActivity {
         buildRecyclerView();
     }
 
-    public void removeItem(int pos) {
+    public void removeItem(final int pos) {
         //first get the key aka location in the server of the account that we are deleting
         String accountKeyToDelete = list.get(pos).getAccountKey();
 
@@ -49,17 +50,39 @@ public class AdminAccounts extends AppCompatActivity {
         DatabaseReference accountReference;
 
         //delete depending on the type of account
-        if (accountType == 0){
+        if (accountType == 0) {
             accountReference = FirebaseDatabase.getInstance().getReference("Customer Accounts").child(accountKeyToDelete);
             accountReference.removeValue();
-        }else if (accountType == 1){
+            //remove the account in the recycle view list
+            list.remove(pos);
+            aAdapter.notifyItemRemoved(pos);
+        } else if (accountType == 1) {
             accountReference = FirebaseDatabase.getInstance().getReference("Employee Accounts").child(accountKeyToDelete);
             accountReference.removeValue();
+            DatabaseReference branchReference = FirebaseDatabase.getInstance().getReference().child("Branches/");
+            branchReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot child : snapshot.getChildren()) {
+                        Branch temp = child.getValue(Branch.class);
+                        if (temp.getBranchID() == list.get(pos).getBranchID()) {
+                            child.getRef().setValue(null);
+                            //remove the account in the recycle view list
+                            list.remove(pos);
+                            aAdapter.notifyItemRemoved(pos);
+                            break;
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
         }
 
-        //remove the account in the recycle view list
-        list.remove(pos);
-        aAdapter.notifyItemRemoved(pos);
+
     }
 
     public void createList() { //format for AccountItem is (int imageResource, int accountID, String firstName, String lastName, String username, OPTIONAL [int branchID])
@@ -70,13 +93,15 @@ public class AdminAccounts extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Iterable<DataSnapshot> children = snapshot.getChildren(); //gets an iterable of the service
-                for (DataSnapshot child: children){
+                for (DataSnapshot child : children) {
                     CustomerAccount tempCustomer = child.getValue(CustomerAccount.class);
                     list.add(new AccountItem(R.drawable.user_icon, tempCustomer));
                 }
             }
+
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {}
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
 
         });
 
@@ -86,7 +111,7 @@ public class AdminAccounts extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Iterable<DataSnapshot> children = snapshot.getChildren(); //gets an iterable of the service
-                for (DataSnapshot child: children){
+                for (DataSnapshot child : children) {
                     EmployeeAccount tempEmployee = child.getValue(EmployeeAccount.class);
                     list.add(new AccountItem(R.drawable.user_icon, tempEmployee));
                 }
