43ed98fe0da603eb1a3fd07592a218872f400c9e
Shang-Lin <38915581+Shang603@users.noreply.github.com>
2020-12-05 21:20:04 -0500

    Deletes services in branches that no longer exist

--- app/src/main/java/com/example/service_novigrad/ui/admin/AdminAccounts.java
+++ app/src/main/java/com/example/service_novigrad/ui/admin/AdminAccounts.java
@@ -12,6 +12,7 @@ import com.example.service_novigrad.accounts.CustomerAccount;
 import com.example.service_novigrad.accounts.EmployeeAccount;
 import com.example.service_novigrad.ui.accounts.AccountItem;
 import com.example.service_novigrad.ui.accounts.AccountsAdapter;
+import com.example.service_novigrad.ui.register.Branch;
 import com.example.service_novigrad.ui.services.ServiceItem;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -39,7 +40,7 @@ public class AdminAccounts extends AppCompatActivity {
         buildRecyclerView();
     }
 
-    public void removeItem(int pos) {
+    public void removeItem(final int pos) {
         //first get the key aka location in the server of the account that we are deleting
         String accountKeyToDelete = list.get(pos).getAccountKey();
 
@@ -55,11 +56,31 @@ public class AdminAccounts extends AppCompatActivity {
         }else if (accountType == 1){
             accountReference = FirebaseDatabase.getInstance().getReference("Employee Accounts").child(accountKeyToDelete);
             accountReference.removeValue();
+            //deleting the corresponding branch class before also deleting it from the list
+            DatabaseReference branchRef = FirebaseDatabase.getInstance().getReference("Branches");
+            branchRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for(DataSnapshot child: snapshot.getChildren()){
+                        Branch temp = child.getValue(Branch.class);
+                        if (temp.getBranchID() == list.get(pos).getBranchID()){
+                            child.getRef().removeValue();
+                        }
+                    }
+
+                    //remove the account in the recycle view list
+                    list.remove(pos);
+                    aAdapter.notifyItemRemoved(pos);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
         }
 
-        //remove the account in the recycle view list
-        list.remove(pos);
-        aAdapter.notifyItemRemoved(pos);
+
     }
 
     public void createList() { //format for AccountItem is (int imageResource, int accountID, String firstName, String lastName, String username, OPTIONAL [int branchID])
