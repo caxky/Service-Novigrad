95054da15df3302dff076437e4582f6befbbe789
Rhiannon@07 <71230352+m3alij@users.noreply.github.com>
2020-12-06 23:17:34 -0500

    Employee service request review functionality

--- app/src/main/java/com/example/service_novigrad/ui/employee/ServiceRequestInformation.java
+++ app/src/main/java/com/example/service_novigrad/ui/employee/ServiceRequestInformation.java
@@ -1,16 +1,26 @@
 package com.example.service_novigrad.ui.employee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.service_novigrad.R;
 import com.example.service_novigrad.ui.customer.CustomerFormRequest;
 import com.example.service_novigrad.ui.services.FieldsAndAttachments;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class ServiceRequestInformation extends AppCompatActivity {
@@ -21,10 +31,19 @@ public class ServiceRequestInformation extends AppCompatActivity {
     CustomerFormRequest customer;
     ServiceRequestItem req;
 
+    //database storage reference
+    private StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employee_service_request_information);
+
+        //Replace with real customerFormRequest object
+        req = this.getIntent().getParcelableExtra("serviceRequest");
+
+        //Storage Reference
+        storageReference = FirebaseStorage.getInstance().getReference(req.getServiceName());
 
         //Associating the textviews with the appropriate IDs
         firstName = findViewById(R.id.empFormReviewFirstName);
@@ -48,12 +67,12 @@ public class ServiceRequestInformation extends AppCompatActivity {
         imageSIN = findViewById(R.id.empFormReviewImageSIN);
         imagePR = findViewById(R.id.empFormReviewImagePR);
 
-        //Replace with real customerFormRequest object
-        req = this.getIntent().getParcelableExtra("serviceRequest");
+
 
         //Initialize the form's value
         initializeFields(req.getForm().getBooleanFields());
-        initializeImages(req.getForm().getAttachments());
+        //initializeImages(req.getForm().getAttachments());
+
 
     }
     public void initializeFields(FieldsAndAttachments list){
@@ -144,5 +163,138 @@ public class ServiceRequestInformation extends AppCompatActivity {
     }
 
     public void initializeImages(Map<String, String> list){
+        if(req.getForm().getAttachments().containsKey("Proof of Status")){
+            StorageReference refPOS = storageReference.child(req.getForm().getAttachments().get("First Name")+"/filePOS.jpg");
+            POS = Uri.parse(list.get("Proof of Status"));
+            refPOS.getFile(POS)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), POS);
+                                imagePOS.setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+        }
+        if(req.getForm().getAttachments().containsKey("Drivers License")){
+            DL = Uri.parse(list.get("Drivers License"));
+            StorageReference refPOS = storageReference.child(req.getForm().getAttachments().get("First Name")+"/fileDL.jpg");
+            refPOS.getFile(DL)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), DL);
+                        imageDL.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+        }
+        if(req.getForm().getAttachments().containsKey("Birth Certificate")){
+            BC = Uri.parse(list.get("Birth Certificate"));
+            StorageReference refPOS = storageReference.child(req.getForm().getAttachments().get("First Name")+"/fileBC.jpg");
+            refPOS.getFile(BC)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), BC);
+                                imageBC.setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+        }
+        if(req.getForm().getAttachments().containsKey("Photo of Customer")){
+            Photo = Uri.parse(list.get("Photo of Customer"));
+            StorageReference refPOS = storageReference.child(req.getForm().getAttachments().get("First Name")+"/filePhoto.jpg");
+            /*Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Photo);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            imagePhoto.setImageBitmap(bitmap);*/
+            refPOS.getFile(Photo)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Photo);
+                                imagePhoto.setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+        }
+        if(req.getForm().getAttachments().containsKey("SIN")){
+            SIN = Uri.parse(list.get("SIN"));
+            StorageReference refPOS = storageReference.child(req.getForm().getAttachments().get("First Name")+"/fileSIN.jpg");
+            refPOS.getFile(SIN)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), SIN);
+                                imageSIN.setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+        }
+        if(req.getForm().getAttachments().containsKey("Proof of Residence")){
+            PR = Uri.parse(list.get("Proof of Residence"));
+            StorageReference refPOS = storageReference.child(req.getForm().getAttachments().get("First Name")+"/filePR.jpg");
+            refPOS.getFile(PR)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), PR);
+                                imagePR.setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+        }
     }
 }
