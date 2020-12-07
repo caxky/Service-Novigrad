8b3a45e2d80efb38f42c70c53bcb7637a8a585e4
caxky <k_james7@outlook.com>
2020-12-06 23:09:49 -0500

    Fixed changing values JUnit

--- app/src/test/java/com/example/service_novigrad/ChangingValuesTest.java
+++ app/src/test/java/com/example/service_novigrad/ChangingValuesTest.java
@@ -3,6 +3,8 @@ package com.example.service_novigrad;
 import com.example.service_novigrad.accounts.CustomerAccount;
 import com.example.service_novigrad.accounts.EmployeeAccount;
 import com.example.service_novigrad.ui.accounts.AccountItem;
+import com.example.service_novigrad.ui.customer.BranchServiceItem;
+import com.example.service_novigrad.ui.customer.CustomerFormRequest;
 import com.example.service_novigrad.ui.employee.ServiceRequestItem;
 import com.example.service_novigrad.ui.register.Branch;
 import com.example.service_novigrad.ui.services.FieldsAndAttachments;
@@ -10,13 +12,16 @@ import com.example.service_novigrad.ui.services.ServiceItem;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class ChangingValuesTest {
     @Test
     public void changeServiceItem(){
         FieldsAndAttachments healthCardFandA = new FieldsAndAttachments("Health Card Service");
-        ServiceItem testServiceItem = new ServiceItem("name", "type", "123", healthCardFandA);
+        ServiceItem testServiceItem = new ServiceItem("name", "type", "123", healthCardFandA, 25.00);
 
         assertEquals("type", testServiceItem.getServiceType());
 
@@ -27,7 +32,12 @@ public class ChangingValuesTest {
 
     @Test
     public void changeServiceRequestItem(){
-        ServiceRequestItem testServiceRequestItem = new ServiceRequestItem(1, "serviceNameTest", "userNameTest");
+        FieldsAndAttachments faa = new FieldsAndAttachments("Health Card Service");
+        Map<String, String> testMap = new HashMap<String, String>();
+        BranchServiceItem bsi = new BranchServiceItem(555, "Test Service Name", "453453g");
+
+        CustomerFormRequest cfr = new CustomerFormRequest(faa, testMap, testMap, bsi);
+        ServiceRequestItem testServiceRequestItem = new ServiceRequestItem(1, "serviceNameTest", cfr);
 
         assertEquals(false, testServiceRequestItem.getStatus());
         assertEquals("serviceNameTest", testServiceRequestItem.getServiceName());
@@ -40,7 +50,7 @@ public class ChangingValuesTest {
 
     @Test
     public void changeBranch(){
-        Branch testBranch = new Branch("testkey", "1", "2", "3", "4", "5", "6", 123, "1234567890", "test@email.com");
+        Branch testBranch = new Branch("testkey", "1", "2", "3", "4", "5", "6", "1", "2", "3", "4", "5", "6", "7", "8", 123, "1234567890", "test@email.com");
 
         assertEquals("1234567890", testBranch.getPhoneNumber());
         assertEquals("test@email.com", testBranch.getEmailAddress());
